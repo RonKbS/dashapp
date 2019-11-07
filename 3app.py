f1fbf2cc22b937f1958c894c51ca1c73e466a613
RonKbS <stephen.bamwite@gmail.com>
2019-11-07 09:23:04 +0000

    use of dropdown to change graph output

--- 3app.py
+++ 3app.py
@@ -3,44 +3,56 @@ import datetime
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 
 
 # export FLASK_ENV=development
 app = dash.Dash()
 
-
-start = datetime.datetime(2019, 1, 6)
-end = datetime.datetime(2019, 11, 6)
-
-df = web.DataReader('^DJI', 'stooq')
-
-# print(df.head())
-
-# df.reset_index(inplace=True)
-# df.set_index('Date', inplace=True)
-# df = df.drop('Symbol', axis=1)
-# import pdb; pdb.set_trace()
-# print(df.head())
-# print(df.info())
+# https://stooq.com/t/
+stook_indices = [
+    {'label': 'HANG SENG', 'value': '^TWSE'},
+    {'label': 'DOW JONES INDU', 'value': '^DJI'},
+    {'label': 'NIKKEI225', 'value': '^NKX'},
+    {'label': 'JCI', 'value': '^JCI'},
+    {'label': 'ALL ORDINARIES', 'value': '^AOR'}
+]
 
 app.layout = html.Div(
     children=[
-        html.H1('wtf is DJI'),
-        dcc.Graph(
-            id='example',
-            figure={
-                'data': [
-                    {'x': df.index, 'y':df.Close, 'type': 'line', 'name': 'DJI'},
-                ],
-                'layout': {
-                    'title': 'DJI'
-                }
-            }
-        )
+        html.Div(
+            children='''symbol to graph'''
+        ),
+        dcc.Dropdown(
+            id='dropdown',
+            value='^AOR',
+            options=stook_indices
+        ),
+        html.Div(id='output-graph')
     ]
 )
 
+
+@app.callback(
+    Output(component_id='output-graph', component_property='children'),
+    [Input(component_id='dropdown', component_property='value')]
+)
+def update_value(value):
+    df = web.DataReader(value, 'stooq')
+    
+    return dcc.Graph(
+        id='stooq',
+        figure={
+            'data': [
+                {'x': df.index, 'y':df.Close, 'type': 'line', 'name': value},
+            ],
+            'layout': {
+                'title': value
+            }
+        }
+    )
+
+
+
 if __name__ == '__main__':
-
-
     app.run_server(debug=True, host='0.0.0.0')
